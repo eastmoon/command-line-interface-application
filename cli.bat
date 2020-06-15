7a3f4b306350f8b097abaefedf324d190fa382a6
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2020-06-16 03:34:25 +0800

    Add CLI application for window batch script

---
+++ cli.bat
@@ -0,0 +1,163 @@
+@rem
+@rem Copyright 2020 the original author jacky.eastmoon
+@rem
+
+:: ------------------- batch setting -------------------
+@rem setting batch file
+@rem ref : https://www.tutorialspoint.com/batch_script/batch_script_if_else_statement.htm
+@rem ref : https://poychang.github.io/note-batch/
+@echo off
+setlocal
+setlocal enabledelayedexpansion
+
+:: ------------------- declare CLI file variable -------------------
+@rem Directory = %~dp0
+@rem Object Name With Quotations=%0
+@rem Object Name Without Quotes=%~0
+@rem Bat File Drive = %~d0
+@rem Full File Name = %~n0%~x0
+@rem File Name Without Extension = %~n0
+@rem File Extension = %~x0
+
+set CLI_DIRECTORY=%~dp0
+set CLI_FILE=%~n0%~x0
+set CLI_FILENAME=%~n0
+set CLI_FILEEXTENSION=%~x0
+
+:: ------------------- declare CLI variable -------------------
+
+set BREADCRUMB=cli
+set COMMAND=
+set COMMAND_BC_AGRS=
+set COMMAND_AC_AGRS=
+
+:: ------------------- declare variable -------------------
+
+@rem retrieve project name
+@rem Ref : https://www.robvanderwoude.com/ntfor.php
+for %%a in ("%cd%") do (
+    set PROJECT_NAME=%%~na
+)
+set PROJECT_ENV=dev
+
+:: ------------------- execute script -------------------
+call :main %*
+goto end
+
+:: ------------------- declare function -------------------
+:main (
+    call :argv-parser %*
+    call :%BREADCRUMB%-args %COMMAND_BC_AGRS%
+    call :main-args %COMMAND_BC_AGRS%
+    IF defined COMMAND (
+        set BREADCRUMB=%BREADCRUMB%-%COMMAND%
+        call :main %COMMAND_AC_AGRS%
+    ) else (
+        call :%BREADCRUMB%
+    )
+    goto end
+)
+:main-args (
+    for %%p in (%*) do (
+        if "%%p"=="-h" ( set BREADCRUMB=%BREADCRUMB%-help )
+        if "%%p"=="--help" ( set BREADCRUMB=%BREADCRUMB%-help )
+    )
+    goto end
+)
+:argv-parser (
+    set COMMAND=
+    set COMMAND_BC_AGRS=
+    set COMMAND_AC_AGRS=
+    set is_find_cmd=
+    for %%p in (%*) do (
+        IF NOT defined is_find_cmd (
+            echo %%p | findstr /r "\-" >nul 2>&1
+            if errorlevel 1 (
+                set COMMAND=%%p
+                set is_find_cmd=TRUE
+            ) else (
+                set COMMAND_BC_AGRS=!COMMAND_BC_AGRS! %%p
+            )
+        ) else (
+            set COMMAND_AC_AGRS=!COMMAND_AC_AGRS! %%p
+        )
+    )
+    goto end
+)
+
+:: ------------------- Main mathod -------------------
+
+:cli (
+    goto cli-help
+)
+
+:cli-args (
+    for %%p in (%*) do (
+        if "%%p"=="--prod" ( set PROJECT_ENV=prod )
+    )
+    goto end
+)
+
+:cli-help (
+    echo This is a Command Line Interface with project %PROJECT_NAME%
+    echo If not input any command, at default will show HELP
+    echo.
+    echo Options:
+    echo      --help, -h        Show more information with CLI.
+    echo      --pord            Setting project environment with "prod", default is "dev"
+    echo.
+    echo Command:
+    echo      up                Startup Server.
+    echo      down              Close down Server.
+    echo.
+    echo Run 'cli [COMMAND] --help' for more information on a command.
+    goto end
+)
+
+:: ------------------- Command "up" mathod -------------------
+
+:cli-up (
+    echo ^> Server UP with %PROJECT_ENV% environment
+    goto end
+)
+
+:cli-up-args (
+    goto end
+)
+
+:cli-up-help (
+    echo This is a Command Line Interface with project %PROJECT_NAME%
+    echo Startup Server
+    echo.
+    echo Options:
+    echo      --help, -h        Show more information with UP Command.
+    goto end
+)
+
+:: ------------------- Command "down" mathod -------------------
+
+:cli-down (
+    echo ^> Server DOWN
+    goto end
+)
+
+:cli-down-args (
+    goto end
+)
+
+:cli-down-help (
+    echo This is a Command Line Interface with project %PROJECT_NAME%
+    echo Close down Server
+    echo.
+    echo Options:
+    echo      --help, -h        Show more information with UP Command.
+    goto end
+)
+
+
+
+:: ------------------- End method-------------------
+
+:end (
+    endlocal
+)
