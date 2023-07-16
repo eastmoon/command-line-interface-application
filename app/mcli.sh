845806442d478f76d80df3ac4d7d0c071de47e8a
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2023-07-16 20:57:15 +0800

    refactor: don't recursive depth search command

--- app/mcli.sh
+++ app/mcli.sh
@@ -204,11 +204,11 @@ function cli-help() {
 function command-description() {
     TARGET_FILE_PATTERN=".*/[^-]*\.sh"
     [ ! -z ${1} ] && TARGET_FILE_PATTERN=".*/${1%.*}-[^-]*\.sh"
-    if [ $(find ${CLI_SHELL_DIRECTORY} -type f -regex "${TARGET_FILE_PATTERN}" | wc -l) -gt 0 ];
+    if [ $(find ${CLI_SHELL_DIRECTORY} -maxdepth 1 -type f -regex "${TARGET_FILE_PATTERN}" | wc -l) -gt 0 ];
     then
         echo ""
         echo "Command: "
-        for file in $(find ${CLI_SHELL_DIRECTORY} -type f -regex "${TARGET_FILE_PATTERN}")
+        for file in $(find ${CLI_SHELL_DIRECTORY} -maxdepth 1 -type f -regex "${TARGET_FILE_PATTERN}")
         do
             COMMAND_NAME=${file##*/}
             COMMAND_NAME=${COMMAND_NAME##*-}
@@ -216,7 +216,7 @@ function command-description() {
             printf "    %-10s        %-40s\n" "${COMMAND_NAME}" "$(source ${file} short)"
         done
         echo ""
-        echo "Run '${CLI_FILENAME} [COMMAND] --help' for more information on a command."
+        echo "Run '${CLI_FILENAME##*/} [COMMAND] --help' for more information on a command."
     fi
 }
 
